6d69e55025b378b62371a008169cbb19daab04d9
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2023-01-14 14:49:17 -0500

    initial commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Leo Stepanewk on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
