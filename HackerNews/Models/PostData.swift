24a1d53264e271168b8de34bcfee0ba55a3fe115
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2023-01-20 10:31:46 -0500

    networking

---
+++ HackerNews/Models/PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  HackerNews
+//
+//  Created by Leo Stepanewk on 1/20/23.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String
+}
